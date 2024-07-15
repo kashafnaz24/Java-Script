ed22ede4ac558a2f673ad0e345314ca3757c4e5a
kashafnaz24 <kashafnazk389@gmail.com>
2024-07-15 15:17:31 +0500

    Java Script

---
+++ script.js
@@ -0,0 +1,517 @@
+
+
+
+
+// let username = prompt("your name is");
+// console.log(username);
+
+
+// let a = 10;
+
+// //post increment
+
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+
+// console.log(a--);
+// console.log(a--);
+// console.log(a--);
+// console.log(a--);
+
+// //pre increment
+
+// console.log(--a);
+// console.log(--a);
+// console.log(--a);
+// console.log(--a);
+
+// let y = 12;
+// let n = 86;
+// console.log(y!==n)
+
+// let mode = "dark";
+// let color = "white";
+// if(mode == "dark"){
+//     console.log(mode)
+// }
+// else{
+//     console.log(yellow)
+// }
+// let age = +prompt("enter your age");
+// if(age > 18){
+//     console.log("you are eligible for vote")
+// }
+// else if(age > 78){
+//     console.log("you are expire")
+// }
+// else{
+//     console.log("you are not eligible for vote")
+// }
+
+// let number = +prompt("enter your number");
+// if(number % 2 == 0){
+//     console.log(number, "is a even number"  )
+// }
+// else{
+//     console.log(number, "is a odd number")
+// }
+
+//date :15-5-2024
+
+// let num = 79 ;
+// if (num => 80 && num <=100) {
+//     console.log("A+ grade");
+// }
+// else if (num > 70) {
+//     console.log("A grade")
+// }
+// else if (num > 60) {
+//     console.log("B+ grade")
+// }
+// else if (num > 50) {
+//     console.log("B grade")
+// }
+// else if (num > 40) {
+//     console.log("C grade")
+// }
+// else if (num > 30) {
+//     console.log("D grade")
+// }
+// else {
+//     console.log("failled")
+// }
+//class work
+
+//1.	Imagine you have a variable age representing a person's age. Write a conditional
+ //statement that checks if the person is eligible to vote. If the person's age is 18 or older, print 'You are eligible to vote.'
+ //If the person's age is less than 18, print 'You are not eligible to vote yet.' Write this in the programming language of your choice.
+
+//  let age = +prompt("enter your age..");
+//  if(age > 18){
+//     console.log("you are egligible for vote")
+//  }
+//  else{
+//     console.log("you are not egligible for vote")
+//  }
+
+ //2.	Consider a variable temperature representing the temperature in Celsius.
+ // Write a conditional statement that checks if the temperature is greater than 25 degrees.
+   //If it is, print 'It's a hot day!' If it's not greater than 25 degrees, print 'It's a normal day.
+   //Write this in the programming language of your choice.
+
+//    let temperature = +prompt("what is the temperature");
+//    if(temperature > 25){
+//     console.log("It's not a hot day")
+//    }
+//    else{
+//     console.log("It's normal day")
+// }
+
+//3.	Imagine you have a variable grade representing a student'
+//s score on a test. Write a conditional statement that checks 
+//if the grade is greater than or equal to 60. If it is, print 'You passed the test!' 
+//If it's less than 60, print 'You failed the test.' Write this in the programming language of your choice
+
+// let grade = +prompt("what is your scorse in result" );
+// if(grade => 60){
+//     console.log("you passed the Exams")
+// }
+// else{
+//     console.log("you Failled in your Exams")
+// }
+
+//4.	Write a conditional statement that checks if a variable is_raining is true. 
+//If it is, print 'Remember to bring an umbrella.' If it's false, print 'enjoy the sunny weather!'.
+
+// let cloud = "raining";
+// let sun = "sunny weather"
+// if(cloud == "raining" ){
+//     console.log("remember to bring an umbrella")
+// }
+// else{
+//     console.log("enjoy the suuny weather")
+// }
+
+//5.	Create a program that asks the user to enter their age.
+ //If the age is greater than or equal to 18, print 'You are an adult.'
+  //Otherwise, print 'You are a minor.
+
+  // let age1 = +prompt("enter your age..");
+  // if(age1 > 18){
+  //   console.log("you are an adult")
+  // }
+  // else{
+  //   console.log("you are Minnor")
+  // }
+
+  //6.	Write a program that asks the user to input a number.
+   //If the number is positive, print 'The number is positive.' 
+  // If it's negative, print 'The number is negative.' If it's zero, print 'The number is zero.' 
+
+  // let numb = +prompt("input a number");
+  // if(numb > 0  ){
+  //   console.log("the number is positive")
+  // }
+  // else if(numb < 0){
+  //   console.log("It's negative number")
+  // }
+  // else if(numb == 0){
+  //   console.log("It's a zero")
+  // }
+  // else{
+  //   console.log("it's not a number")
+  // }
+
+//done the work
+
+//   &&    both values are true
+//   ||    one value is true while other is false
+//   !     value is  true it works value is false vice versa 
+
+
+//question 01
+// let city = +prompt("IN WHICH CITY YOU ENTER");
+// //let City = "Karachi";
+// if(city === "karachi"){
+//   console.log("wellcome to the city of light")
+// } 
+
+// else{
+//   console.log("wellcome to the city")
+// }
+
+//question 02
+// let Gender = +prompt("what is your gender");
+
+// if(Gender === "male"){
+//   console.log("good morning Sir")
+// } 
+//  else if(Gender === "female" ){
+//   console.log("good morning ma'am")
+// } 
+// else{
+//   console.log("wellcome")
+// }
+
+//question 03
+// let color = +prompt("trafic light signal");
+// if(color ==='red'){
+//   console.log("must stop")
+// }
+// else if( color ==='yellow'){
+//   console.log("redy to move")
+// }
+// else if(color ==='green'){
+//   console.log("move now")
+// }
+// else{
+//   console.log("trafic light problem")
+// }
+
+  // ARRAYS........
+
+// let fristname =[ "kashaf","maryum","asra","nimra","wania","abeera","zarqa"];
+// console.log(fristname);
+
+// let Fristname =[ "kashaf","maryum","asra","nimra","wania","abeera","zarqa"];
+// console.log(Fristname[1].length);
+
+// let number = [2,3,4,6,7,8,9,10];
+// console.log(number);
+
+// let Numb = [2,3,4,6,7,8,9,10];
+// console.log(Numb[5].length );
+
+// let info = ["rameen",23,"yusra","tehreem", 89 , 90];
+// console.log(info[0].length );
+
+/*let mixedarry =["rameen",23,"yusra", 67 ,true,"3568569"];
+console.log(mixedarry[0].length);
+console.log(mixedarry[1].length);
+console.log(mixedarry[2].length);
+console.log(mixedarry[3].length);
+console.log(mixedarry[4].length);
+console.log(mixedarry[5].length);*/
+
+// ARRAYS METHOD  () ........ tree methods of string
+// 1 PUSH..addition in the array in last
+
+// let fristname =[ "kashaf","maryum","asra","nimra","wania","abeera","zarqa"];
+//  console.log(fristname.push("pakistan","india"));
+//  console.log(fristname);
+
+//2 pop..subtrat in the arry in last
+
+
+//let fristname =[ "kashaf","maryum","asra","nimra","wania","abeera","zarqa","pakistan","india"];
+ //console.log(fristname.pop("india"));
+//console.log(fristname);
+
+// 3 tostring...
+
+
+
+//let fristname =[ "kashaf","maryum","asra","nimra","wania","abeera","zarqa","pakistan","india"];
+//console.log(fristname)
+ //console.log(fristname.toString())
+
+
+ // 4 concat :join two variable
+
+/*const mygirls = ["raviya","laiba"]
+const myboys =["Ali","ahmed"] 
+const mychildern = myboys.concat(mygirls);
+console.log(mychildern)*/
+
+// 5 unshift   : add in the arry in frist
+
+// const fruits =["banana","apple","grappes"]
+// console.log(fruits.unshift("mango","apple","grappes"));
+// console.log(fruits);
+
+// const fruit =["banana","apple","grappes"]
+// fruit.unshift("mango","apple","grappes");
+// console.log(fruit);
+
+// 6 shift : subtract in the arry in frist
+
+// const fruit1 =["banana","apple","grappes"]let
+// console.log(fruit1);
+// console.log(fruit1.shift("banana")); //this is showed
+// fruit1.shift()        // this is not showed
+// console.log(fruit1);
+
+// 7 slice : returns the piece of the Array
+
+// let vegetable =["tomato", "potato","leaf","wheat","rice","chicken"]
+// console.log(vegetable)
+//console.log(vegetable.slice(1))
+
+// let piece_of_Array = vegetable.slice(1,4); //left side
+// console.log(piece_of_Array)
+
+
+// let piece_of = vegetable.slice(-6,-2); //right side
+// console.log(piece_of)
+
+// let piece = vegetable.slice(-5,3); //right side
+// console.log(piece)
+ 
+// 8 splice : add ,remove and replace
+// splice(start, remove, new)
+
+//let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+//let days = month.splice(2,2,"tuesday", "monday")
+//console.log(month)
+//console.log(days)
+
+
+/*Qs. Create an array to store companies-> “Bloomberg”, “Microsoft”, “Uber”, “Google”, “IBM”, “Netflix”
+
+Remove the first company from the array
+
+Remove Uber & Add Ola in its place
+
+Add Amazon at the end*/
+
+// let company = ["Bloomberg" ,"Microsoft", "Uber", "Google", "IBM", "Netflix"]
+// console.log(company.shift(0))
+// console.log(company)
+
+// let companies =["Bloomberg" ,"Microsoft", "Uber", "Google", "IBM", "Netflix"]
+// console.log(companies.splice(2,1,"ola"))
+// console.log(companies)
+
+// let companies1 = ["Bloomberg" ,"Microsoft", "Uber", "Google", "IBM", "Netflix"]
+// console.log(companies1.push("amazon"))
+// console.log(companies1)
+
+//let piece1 = ["Avocado", "Banana", "Apple", "Mango", "Kiwi", "Orange", "Pineapple", "Watermelon", "Lemon", "Apricot", "Blueberry", "Plum", "Cherry", "Fig", "Papaya", "Grapefruit" , "Strawberry"]
+//console.log(piece1.splice(1,6))
+
+
+//1.Declare an empty array using JS literal notation to store
+//student names in future.
+//Declare and initialize a strings array.
+//2. Declare and initialize a numbers array.
+//3. Declare and initialize a boolean array.
+//4. Declare and initialize a mixed array.
+
+//q1
+//let student = []
+//console.log(student)
+
+//q2
+//let string1 = ["Avocado", "Banana", "Apple", "Mango", "Kiwi", ]
+//console.log(string1)
+
+//q3
+//let number1 = [4,5,64,443,7]
+//console.log(number1)
+
+//q4
+//let boolean =[true, false]
+//console.log(boolean)
+
+//q5
+// let mixed = ["Avocado", "Banana",35, true,"Avocado", "Banana",false]
+// console.log(mixed)
+
+//q6
+
+/*Declare and Initialize an array and store available
+education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+BS, BCOM, MS, M. Phil., PhD). Show the listed
+qualifications in your browser like:*/
+
+
+// let educationqualifications = ['SSC', 'HSC', 'BCS','BS', 'BCOM', 'MS', 'M.Phil', 'PhD']
+// console.log('Education Qualifications In Pakistan')
+// console.log(educationqualifications)
+
+//q7
+
+// let studentname = ["rabia","kashaf","hiba"];
+// let studentscore = [320,230,240];
+// let totel= 500;
+// let percentage1 = (studentscore[0] / totel) * 100;
+// let percentage2 = (studentscore[1] / totel ) *100;
+// let percentage3 = (studentscore[2] / totel )* 100;
+// document.write("<br>"+'score of' +  "  " + studentname[0] + "  " + 'is' +"  " + studentscore[0] + "  " + 'percentage:' + percentage1+"%")
+// document.write("<br>"+'score of' +  "  " + studentname[1] + "  " + 'is' +"  " + studentscore[1] + "  " + 'percentage:' + percentage2+"%")
+// document.write("<br>"+'score of' +  "  " + studentname[2] + "  " + 'is' +"  " + studentscore[2] + "  " + 'percentage:' + percentage3+"%")
+
+
+// for loops
+
+// for (let i = 0 ;i < 10; i++){
+// console.log("hello world")
+// }            l 
+
+// for(let i = 5 ; i < 51 ; i++){
+//   console.log('table of 5');
+// }
+
+// for(let i = 50; i > 4; i = i-5){
+//   console.log('table of 5');
+// }
+
+// let fruit = ["apple","banana","mango","orange","strawberry"]
+// document.write(fruit)
+
+
+// let hsd = "counting:"
+// document.write( "<h1>",hsd+"    "+"    ")
+// let con = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+// document.write(con)
+// document.write("counting:")
+// for(let i =1 ;i<15; i++){
+//   document.write( i +",")
+// }
+
+// document.write("<br>"+ "reverse counting")
+// for(let i =10 ;i<14; i--){
+//   document.write( i +",")
+// }
+
+//chapter 41
+
+//while loop
+
+// let n=+prompt("enter the value of n")
+// let i = 0;
+// while(i <= n){
+//   document.write("<br>"+ i);
+//   i++;
+// }
+
+//"write a program to display sum of numbers Form 1 t0 100"
+
+// let sum = 0;
+// let num = 1;
+// while(num <= 100){
+//   sum += num ;
+//   num++;
+
+// }
+// document.write("sum of 1 to 100: " + sum)
+ 
+//other example 
+//"write a program to display sum of numbers Form 3 t0 70"
+// let sum1 = 2;
+// let num1 = 3;
+// while(num1 <= 70){
+//   sum1 += num1;
+//   num1++;
+// } 
+// document.write("sum of 3 to 70: " + sum)
+
+//"write a program to display factorial of a number
+
+// let number = 4
+// let factorial = 1
+// while(number>0){
+//   factorial = factorial * number;
+//   number++;
+// }
+// document.write("factorial of a number :"+ factorial)
+
+//finding the frist 10 even number
+
+// let numb = 1
+// let even = 2
+// while(numb<=20){
+//   even = even * numb
+//   numb++
+
+// }
+// console.log("the frist 10 even number"+ numb)
+
+//other example
+// let evennum = []
+// let i =0
+// while(evennum.length<10){
+//   if(i%2==0){
+//     evennum.push(i)
+//   }
+//   i++;
+//}
+//document.write(" the frist 10 even number:"+evennum)
+
+//user input validation
+
+// let userinput;
+// let password ="java78563"
+// while(userinput!==password){
+//   userinput=prompt("enter your password")
+// }
+// document.write('accesss granted')
+
+//generating countdown
+
+// let countdown=10
+// while(countdown>0){
+//   document.write("<br>"+countdown);
+//   countdown--;
+// }
+// document.write("<br>"+"<h1>happy")
+
+
+//do while loop
+
+// let m = +prompt("enter the value of m")
+// let i = 10
+// do{
+//   document.write("<br>"+ i)
+//   i++;
+// }while(i<=m)
+
+let number = 4
+let factorial = 1
+do{
+   factorial = factorial * number;
+    number++;
+  document.write("factorial of a number :"+ factorial)
+}while(number>0)
